a2a60cd86e154ed0e8f765141912210cd365b0d6
Shubhanker Singhal <72600840+shubhanker2603@users.noreply.github.com>
2023-12-09 13:34:57 +0530

    Add files via upload

---
+++ BookMngmtBLL/AccountRepo/IAccountOPs.cs
@@ -0,0 +1,12 @@
+﻿using BookMngmtDAL.Entities;
+using System.Threading.Tasks;
+
+namespace BookMngmtBLL.AccountRepo
+{
+    public interface IAccountOPs
+    {
+        Task<string> PasswordSigninAsync(LogedIn signInModel);
+        Task UpdateUserBorrow(string userId1, string userId2);
+        Task SignOutAsync();
+    }
+}
---
+++ BookMngmtBLL/BooksRepo/IBooksBLLOPs.cs
@@ -0,0 +1,13 @@
+﻿using BookMngmtBLL.BLLModels;
+using System.Collections.Generic;
+
+namespace BookMngmtBLL.BooksRepo
+{
+    public interface IBooksBLLOPs
+    {
+        void AddBook(BookModel eventModel);
+        List<BookModel> GetAllBooks();
+        BookModel GetBookById(int? id);
+        public void EditBook(int id, string Borrower, int rating);
+    }
+}
